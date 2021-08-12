6a210fa1bc8c4ab6c41a22b20680bfc6e7cd7985
adam buxton <abuxton@users.noreply.github.com>
2021-08-12 16:31:36 +0100

    Update main.tf

--- manifests/main.tf
+++ manifests/main.tf
@@ -1,7 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/policy_set
 
 resource "tfe_policy_set" "example" {
-  name          = "var.policy_name"
+  name          = "example_policy"
   description   = "An example policy set"
   organization  = var.org_name
   #policies_path = "policies/my-policy-set"
@@ -9,7 +9,7 @@ resource "tfe_policy_set" "example" {
 
   vcs_repo {
     identifier         = "{var.org_name}/{var.vcs-project-name}"
-    branch             = "master"
+    branch             = "main"
     ingress_submodules = false
     oauth_token_id     = var.tfe_oauth_client.test.id
   }
