302f25754544ce0ecdc55a06ce31280e2e3085db
Adam Buxton <buxton.adam@gmail.com>
2022-02-02 12:21:02 +0000

    add some vars

--- manifests/main.tf
+++ manifests/main.tf
@@ -1,17 +1,17 @@
 # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/policy_set
 
 resource "tfe_policy_set" "example" {
-  name          = "example_policy"
-  description   = "An example policy set"
-  global        = var.global ? true : false
-  organization  = var.org_name
+  name         = "example_policy"
+  description  = "An example policy set"
+  global       = var.global ? true : false
+  organization = var.org_name
   #policies_path = "policies/my-policy-set"
-  workspace_ids = var.global ? [] : var.organizations_workspace_ids 
+  workspace_ids = var.global ? [] : var.organizations_workspace_ids
 
-#   vcs_repo {
-#     identifier         = "{var.vcs_user_name}/{var.vcs_project_name}"
-#     branch             = "main"
-#     ingress_submodules = false
-#     oauth_token_id     = var.tfe_oauth_client.test.id
-#   }
+  #   vcs_repo {
+  #     identifier         = "{var.vcs_user_name}/{var.vcs_project_name}"
+  #     branch             = "main"
+  #     ingress_submodules = false
+  #     oauth_token_id     = var.tfe_oauth_client.test.id
+  #   }
 }
--- manifests/variables.tf
+++ manifests/variables.tf
@@ -1,4 +1,15 @@
-variable "org_name"{
+variable "org_name" {
+  type = string
 }
-variable "tfe_workspace"{
+variable "tfe_workspace" {
+  type = string
+}
+
+variable "organizations_workspace_ids" {
+  type = list(string)
+}
+
+variable "global" {
+type = bool
+default = false
 }
