8240859cf63b4a8b5334ecfe3f349c132d575da7
Adam Buxton <buxton.adam@gmail.com>
2022-08-17 15:06:30 +0100

    remove var.tfe_workspace

--- modules/sentinel/tests/default/variables.tf
+++ modules/sentinel/tests/default/variables.tf
@@ -1,16 +1,13 @@
 variable "org_name" {
   type = string
 }
-variable "tfe_workspace" {
-  type = string
-}
 
 variable "organizations_workspace_ids" {
-  type = list(string)
+  type    = list(string)
   default = null
 }
 
 variable "global" {
-type = bool
-default = false
+  type    = bool
+  default = false
 }
