39aad09acaf0e115acced61cf1a0f66fd19b8f5b
Adam Buxton <buxton.adam@gmail.com>
2022-08-17 15:06:35 +0100

    .

--- modules/sentinel/main.tf
+++ modules/sentinel/main.tf
@@ -7,7 +7,7 @@ resource "tfe_policy_set" "example" {
   organization  = var.org_name
   policies_path = "./config/default"
   workspace_ids = var.global ? [] : var.organizations_workspace_ids
-  policy_slug   = "../../"
+  slug          = "../../"
   #   vcs_repo {
   #     identifier         = "{var.vcs_user_name}/{var.vcs_project_name}"
   #     branch             = "main"
---
+++ modules/sentinel/tests/default/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    tfe = {
+      source = "hashicorp/tfe"
+      #  version = "0.25.3"
+    }
+  }
+}
+
+provider "tfe" {
+  # Configuration options
+}
