253c3b58e83ecc830fb2b59f1e0038b2056090b0
Adam Buxton <buxton.adam@gmail.com>
2022-08-17 15:16:40 +0100

    syntax fix

--- modules/sentinel/tests/default/main.tf
+++ modules/sentinel/tests/default/main.tf
@@ -2,8 +2,8 @@ data "tfe_slug" "default" {
   source_path = "../../"
 }
 module "sentinel" {
-  source   = "../.."
-  org_name = var.org_name
-  global   = true
-  slug     = data.tfe_slug.default
+  source        = "../.."
+  org_name      = var.org_name
+  global        = true
+  tfe_slug_data = data.tfe_slug.default
 }
