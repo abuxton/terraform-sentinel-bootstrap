d744917eaa3da045cb18aba3743ae8ce396262d2
Adam Buxton <buxton.adam@gmail.com>
2022-08-17 14:49:14 +0100

    update for test

--- manifests/main.tf
+++ manifests/main.tf
@@ -1,13 +1,13 @@
 # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/policy_set
 
 resource "tfe_policy_set" "example" {
-  name         = "example_policy"
-  description  = "An example policy set"
-  global       = var.global ? true : false
-  organization = var.org_name
-  #policies_path = "policies/my-policy-set"
+  name          = "example_policy"
+  description   = "An example policy set"
+  global        = var.global ? true : false
+  organization  = var.org_name
+  policies_path = "./config/default"
   workspace_ids = var.global ? [] : var.organizations_workspace_ids
-
+  policy_slug   = "../."
   #   vcs_repo {
   #     identifier         = "{var.vcs_user_name}/{var.vcs_project_name}"
   #     branch             = "main"
